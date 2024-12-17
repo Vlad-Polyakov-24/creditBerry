04caa195c1f3cb06d5ad1fe5248e032a748eabcd
Vladyslav Poliakov <vladyslav.poliakov.work@gmail.com>
2024-12-17 22:23:45 +0200

    fix(handle back): done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { AppLayout } from '@app/layouts/AppLayout';
 import { AppStatus, useChangeStatus } from '@entities/App';
 import { useLocalStorage } from '@entities/User';
-import { localStorageVars } from '@shared/const/localStorage.ts';
+import { localStorageVars } from '@shared/const/localStorage';
 
 const App = () => {
   const { change } = useChangeStatus();
@@ -12,10 +12,6 @@ const App = () => {
     const handleVisibilityChange = () => {
       const isVisited = getStorage(localStorageVars.LOGGED_IN);
 
-      console.log('handleVisibilityChange');
-      console.log('document.visibilityState', document.visibilityState);
-      console.log('window.location.pathname', window.location.pathname);
-
       if (document.visibilityState === 'visible' && isVisited) {
         change({ to: AppStatus.DEFAULT });
       }
@@ -23,9 +19,7 @@ const App = () => {
 
     document.addEventListener('visibilitychange', handleVisibilityChange);
 
-    return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
+    return () => document.removeEventListener('visibilitychange', handleVisibilityChange);
   }, [change, getStorage]);
 
   return <AppLayout />;
