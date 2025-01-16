b0ecfad3e7e269e526ff6f75844fa7ff816d0e9d
Vladyslav Poliakov <vladyslav.poliakov.work@gmail.com>
2025-01-16 18:31:18 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   plugins: [
